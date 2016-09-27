29aabd4e5fd6087062288f67d14a8b7519dacb92
Byrth <squarefocus@hotmail.com>
2016-09-27 12:31:55 -0400

    Make resources_service PC-independent!

--- resources_service/resources_service.lua
+++ resources_service/resources_service.lua
@@ -1,11 +1,11 @@
 require('io')
 require('string')
 share_container = require('sharing')
+windower = require('windower')
 
 resources = share_container()
 
--- REVISIT : NEED A WAY TO GET THE ACTUAL DIRECTORY FOR EVERYONE
-for f in io.popen([[dir "E:\Users\Peter\My Documents\GitHub\windower5\build\bin\debug\packages\resources_service\res\" /b]]):lines() do
+for f in io.popen('dir "'..windower.addon_path..'\\res\\" /b'):lines() do
     f = f:sub(1,-5) -- Removes extension.
     local data = require('res.'..f)
     resources[f] = data
