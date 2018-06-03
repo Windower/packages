6675d65acc3ecb44a00454d2561d593f57859b5b
Nifim <TesaRollens@gmail.com>
2018-06-02 22:04:12 -0700

    Changes to player lib
    changes to reflect changes in service. also corrected formatting

--- player/player.lua
+++ player/player.lua
@@ -2,101 +2,108 @@ local res = require('resources')
 local shared = require('shared')
 local fetch_player = shared.get('player_service', 'player')
 
-local metas = { --meta methods for player tables. these handle indexing for specific table which need addtional index coverage. ex: jobs.Monk, jobs.MNK, jobs[2]
-  
-  jobs = {                                                      
-    index = function(t,k)                                       
-      if type(k) == 'string' then                               
-        k = k:lower()     
-        for _,job in pairs(res.jobs) do            
-          if job.english:lower() == k or job.ens:lower() == k then               
-            return rawget(t,job.id)                             
-          end                                                   
-        end                                                     
-      end                                                       
-      return rawget(t,k)                                        
-    end},
-  nations = {
-    index = function(t,k)
-      local nations = {'Bastok','windurst'}
-      nations[0] = "San d'Oria"
-      if k == 'name' then
-        return nations[t.id] or ''
-      else
-        return rawget(t,k)
-      end
-    end},
-  skills = {
-    index = function(t,k)
-      if type(k) == 'string' then
-        k = k:lower()
-        for _,skill in pairs(res.skills) do
-          if skill.english:lower() == k then
-            return rawget(t,skill.id)        
-          end
+local indexers = {
+    job_levels = function(t, k)
+        if type(k) == 'string' then
+            local job_id = res.jobs:first(function(v) return (v.english == k or v.english_short == k)end).id
+            return rawget(t, job_id) or rawget(t, k)
         end
-      end
-      return rawget(t,k)
-    end},
-  stats = {
-    index = function(t, k)
-      if type(k) == 'string' and rawget(t,k..'_base') then
-        return rawget(t,k..'_base')+rawget(t,k..'_added')
-      end
-      return rawget(t,k)
-    end},
+        return rawget(t, k)
+    end,
+    nations = function(t, k)
+        local nations = {'Bastok','windurst'}
+        nations[0] = "San d'Oria"
+        if k == 'name' then
+            return nations[t.id] or ''
+        else
+            return rawget(t, k)
+        end
+    end,
+    skills = function(t, k)
+        if type(k) == 'string' then
+            local categories = res.skills:where(function(v) return v.category == k end):totable()
+            if #categories ~= 0 then
+                return setmetatable({}, {
+                        __index = function(_, k2)
+                            if type(k2) == 'string' then
+                                local skill_id = res.skills:first(function(v) return (v.english == k2 and v.category == k) end).id
+                                return rawget(t, skill_id) or rawget(t, k2)
+                            end
+                            return rawget(t, k2)
+                        end,
+                        __pairs = function(_)
+                            return function(_, k2)
+                                local k2, v = next(t, k2)
+                                while k2 and res.skills[k2].category ~= k do
+                                    k2, v = next(t, k2)
+                                end
+                                return k2, v
+                            end
+                        end,
+                        __metatable = false,
+                    })
+            end
+            local skill_id = res.skills:first(function(v) return v.english == k end).id
+            if rawget(t, skill_id) ~= nil then
+                return rawget(t, skill_id)
+            end
+        end
+        return rawget(t, k)
+    end,
 }
 
 local get_player_pairs = function(data, key)
-  return {next(data, key)}
+    return {next(data, key)}
 end
 
 local get_player_value = function(data, key)
-  if data[key] ~= nil then
-    return data[key]
-  else 
-    return nil
-  end
+    if data[key] ~= nil then
+        return data[key]
+    else 
+        return nil
+    end
 end
 
 local player = setmetatable({}, {
-    __index = function(t,k)
-      local ok, result = fetch_player(get_player_value, k)
-      if not ok then
-        error(result)
-      end
-      if type(result) == 'table' then
-        return setmetatable({},{
-            __index = function(_,l)               
-              if metas[k] then
-                return metas[k].index(result,l)
-              else
-                return result[l]
-              end
-            end,
-            __newindex = function() error('This value is read-only.') end,
-            __pairs =function(t) 
-              return function(t, k) return next(result, k) end
-            end,
-            __metatable = false
-          })
-      else 
-        return result
-      end
-    end,
-    __newindex = function() error('This value is read-only.') end,
+        __index = function(t, k)
+            local ok, result = fetch_player(get_player_value, k)
 
-    __pairs = function(t)
-      return function(t, k)
-        local ok, result = fetch_player(get_player_pairs, k)
-        if not ok then
-          error(result)              
-        end
-        return unpack(result)
-      end, t, nil
-    end,
+            if not ok then
+                error(result)
+            end
 
-    __metatable = false
+            if type(result) == 'table' then
+                return setmetatable({}, {
+                        __index = function(_, l)
+                            if indexers[k] then
+                                return indexers[k](result, l)
+                            else
+                                return result[l]
+                            end
+                        end,
+                        __newindex = function() error('This value is read-only.') end,
+                        __pairs =function(_) 
+                            return function(_, k)
+                                return next(result, k)
+                            end
+                        end,
+                        __metatable = false,
+                    })
+            else
+                return result
+            end
+        end,
+        __newindex = function() error('This value is read-only.') end,
 
-  })
+        __pairs = function(t)
+            return function(t, k)
+                local ok, result = fetch_player(get_player_pairs, k)
+                if not ok then
+                    error(result)
+                end
+                return unpack(result)
+            end, t, nil
+        end,
+        __metatable = false,
+    })
 return player
