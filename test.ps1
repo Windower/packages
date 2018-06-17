7775a8b64c163127c5e588de4bf3d79a923e30f9
alphaONE2 <1904368+alphaONE2@users.noreply.github.com>
2018-06-17 01:50:20 -0500

    Add reflect package

--- test.ps1
+++ test.ps1
@@ -15,35 +15,39 @@ $luajit = Join-Path $luaDir "bin\luajit.exe"
 
 $passed = $true
 
-Get-ChildItem $stagingDir -Directory -Recurse |
-    Where-Object { $_.Name -ceq ".test" } |
+Get-ChildItem $stagingDir -Directory |
     ForEach-Object {
-        Push-Location $_.FullName
-        Get-ChildItem -File -Filter "*.lua" |
-            ForEach-Object {
-                Push-Location $stagingDir
-                "Running tests $((Resolve-Path $_.FullName -Relative).Replace(".\", ''))..."
-                Pop-Location
-
-                try { $output = & $luajit $_.FullName 2>&1 } catch { }
-                $result = $?
-                $output | ForEach-Object {
-                    $message = ([string]$_).Replace("${luajit}:", "").Trim()
-                    if ($_ -is [System.Management.Automation.ErrorRecord]) {
-                        Write-Host $message -ForegroundColor Red
+        $path = $_.FullName
+        Get-ChildItem $path -Directory -Recurse |
+        Where-Object { $_.Name -ceq ".test" } |
+        ForEach-Object {
+            Push-Location $path
+            Get-ChildItem $_.FullName -File -Filter "*.lua" |
+                ForEach-Object {
+                    Push-Location $stagingDir
+                    "Running tests $((Resolve-Path $_.FullName -Relative).Replace(".\", ''))..."
+                    Pop-Location
+    
+                    try { $output = & $luajit $_.FullName 2>&1 } catch { }
+                    $result = $?
+                    $output | ForEach-Object {
+                        $message = ([string]$_).Replace("${luajit}:", "").Trim()
+                        if ($_ -is [System.Management.Automation.ErrorRecord]) {
+                            Write-Host $message -ForegroundColor Red
+                        } else {
+                            Write-Host $message
+                        }
+                    }
+    
+                    if ($result) {
+                        Write-Host "Tests Passed`n" -ForegroundColor Green
                     } else {
-                        Write-Host $message
+                        Write-Host "Tests Failed`n" -ForegroundColor Red
+                        $script:passed = $false
                     }
                 }
-
-                if ($result) {
-                    Write-Host "Tests Passed`n" -ForegroundColor Green
-                } else {
-                    Write-Host "Tests Failed`n" -ForegroundColor Red
-                    $script:passed = $false
-                }
-            }
-        Pop-Location
+            Pop-Location
+        }
     }
 
 $error.clear()
