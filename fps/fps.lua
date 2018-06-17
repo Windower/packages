d0d737feea90b314b5b0d2e435bfdd8f18dfc5d7
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2018-06-17 21:10:43 +0200

    FPS: New addon

---
+++ fps/fps.lua
@@ -0,0 +1,25 @@
+local settings = require('settings')
+local ui = require('ui')
+local os = require('os')
+local table = require('table')
+local string = require('string')
+
+local defaults = {
+    x = 0,
+    y = 0,
+}
+
+local options = settings.load(defaults)
+
+local times = enumerable.wrap({})
+
+ui.display(function()
+    if #times >= 10 then
+        table.remove(times, 1)
+    end
+    times[#times + 1] = os.clock()
+
+    ui.location(options.y, options.y)
+
+    ui.text(string.format('%.1f', (#times - 1) / (times[#times] - times[1])))
+end)
