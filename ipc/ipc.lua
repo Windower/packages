628ec5ac1a80e445c546956693f6671b1c3eb7e7
alphaONE2 <1904368+alphaONE2@users.noreply.github.com>
2018-06-17 23:25:59 -0500

    Add ipc package

---
+++ ipc/ipc.lua
@@ -0,0 +1,142 @@
+local bit = require('bit')
+local event = require('event')
+local ffi = require('ffi')
+local table = require('table')
+local windower = require('windower')
+
+local data_ptr = ffi.typeof[[struct {
+    int32_t next_id[1];
+    struct {
+        int32_t id[1];
+        uint32_t pid;
+        uint16_t size;
+        char data[506];
+    } messages[127];
+}*]]
+
+ffi.cdef[[
+int CloseHandle(void*);
+void* CreateFileMappingW(void*, void*, unsigned long, unsigned long, unsigned long, wchar_t const*);
+void* CreateMutexW(void*, int, wchar_t const*);
+unsigned int GetCurrentProcessId();
+unsigned long GetLastError();
+long __cdecl InterlockedCompareExchange(long volatile*, long, long);
+long __cdecl InterlockedExchange(long volatile*, long);
+long __cdecl InterlockedIncrement(long volatile*);
+void* MapViewOfFile(void*, unsigned long, unsigned long, unsigned long, size_t);
+int ReleaseMutex(void*);
+int UnmapViewOfFile(void const*);
+unsigned long WaitForSingleObject(void*, unsigned long);
+]]
+
+local c = ffi.C
+
+local mt = {__metatable = false, keep_alive = {}}
+local ipc = setmetatable({}, mt)
+
+local pid = c.GetCurrentProcessId()
+
+ipc.received = event.slim.new()
+
+local mutex
+local data
+do
+    local invalid_handle = ffi.cast('void*', -1)
+
+    -- Doesn't properly handle Unicode, but it doesn't actually matter
+    -- here; the result should still be valid UCS-16 and unique.
+    local wide
+    do
+        local wide_string = ffi.typeof('wchar_t[?]')
+        wide = function(str)
+            local buffer = wide_string(#str + 1)
+            for i = 1, #str do
+                buffer[i - 1] = str:byte(i)
+            end
+            return buffer
+        end
+    end
+
+    local name = wide('windower:' .. windower.version .. ':ipc:' .. (package.name or '[:script:]'))
+
+    do
+        local handle = c.CreateMutexW(nil, 0, name)
+        if handle == nil or mutex_handle == invalid_handle then
+            error('error creating ipc mutex [error code: ' .. c.GetLastError() .. ']')
+        end
+        mutex = ffi.gc(handle, c.CloseHandle)
+    end
+
+    do
+        local handle = c.CreateFileMappingW(invalid_handle, nil, --[[PAGE_READWRITE]] 0x4, 0, 0x10000, name)
+        if handle == nil or handle == invalid_handle then
+            error('error creating ipc memory mapped file [error code: ' .. c.GetLastError() .. ']')
+        end
+        handle = ffi.gc(handle, c.CloseHandle)
+        table.insert(mt.keep_alive, handle)
+
+        local view = c.MapViewOfFile(handle, --[[FILE_MAP_WRITE]] 0x2, 0, 0, 0)
+        if view == nil then
+            error('error creating ipc memory mapped file view [error code: ' .. c.GetLastError() .. ']')
+        end
+        data = ffi.gc(data_ptr(view), c.UnmapViewOfFile)
+    end
+
+    do
+        local coroutine_sleep_frame = coroutine.sleep_frame
+        local bit_band = bit.band
+        local bit_tobit = bit.tobit
+        local ffi_string = ffi.string
+
+        local received = ipc.received
+        local current_id = tonumber(c.InterlockedCompareExchange(data.next_id, 0, 0))
+        coroutine.schedule(function()
+            while true do
+                local next_id = c.InterlockedCompareExchange(data.next_id, 0, 0)
+                while current_id ~= next_id do
+                    local index = bit_band(current_id, 0x7F)
+                    if data.messages[index].pid ~= pid then
+                        local message = ffi_string(data.messages[index].data, data.messages[index].size)
+                        received:trigger(message)
+                    end
+                    current_id = bit_tobit(current_id + 1)
+                    next_id = c.InterlockedCompareExchange(data.next_id, 0, 0)
+                end
+                coroutine_sleep_frame()
+            end
+        end)
+    end
+end
+
+do
+    local bit_band = bit.band
+    local bit_tobit = bit.tobit
+    local ffi_copy = ffi.copy
+    ipc.send = function(message)
+        local size = #message
+        if size > 506 then
+            error('message exceeds maxiumum length of 506 bytes')
+        end
+
+        local lock_result = c.WaitForSingleObject(mutex, --[[INFINITE]] 0xFFFFFFFF)
+        if lock_result == 0x80 then
+            print('WARNING: ipc mutex was abandoned by another process')
+        elseif lock_result == 0xFFFFFFFF then
+            error('error locking ipc mutex [error code: ' .. c.GetLastError() .. ']')
+        end
+
+        local next_id = bit_tobit(data.next_id[0])
+        local index = bit_band(next_id, 0x7F)
+        c.InterlockedExchange(data.messages[index].id, next_id)
+        data.messages[index].pid = pid
+        data.messages[index].size = size
+        ffi_copy(data.messages[index].data, message, size)
+        c.InterlockedIncrement(data.next_id)
+
+        if c.ReleaseMutex(mutex) == 0 then
+            error('error releasing ipc mutex [error code: ' .. c.GetLastError() .. ']')
+        end
+    end
+end
+
+return ipc
