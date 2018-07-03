fdb0efb92794158e898681a2d3e3e27fec7f9642
Mark <mgordon52192@gmail.com>
2018-07-03 02:07:37 -0400

    Use windower.client_hwnd

--- window_title/window_title.lua
+++ window_title/window_title.lua
@@ -1,20 +1,12 @@
 local account = require('account')
 local ffi = require('ffi')
-local table = require('table')
-local string = require('string')
+local windower = require('windower')
 
 local user32 = ffi.load('user32')
-local kernel32 = ffi.load('kernel32')
 
 ffi.cdef[[
     bool SetWindowTextW(void* hwnd, wchar_t const* title);
-    typedef int (__stdcall *WNDENUMPROC)(void* hwnd, intptr_t l);
-    bool EnumThreadWindows(uint32_t thread, WNDENUMPROC fn, intptr_t l);
-    int GetClassNameW(void* hwnd, wchar_t const* name, int max_length);
-    uint32_t GetCurrentThreadId();
 ]]
-
-local game_window = false
 
 --[[
     Temporary until we provide a common mechanism for wchar_t support
@@ -28,40 +20,13 @@ local wide = function(str)
     return wstr
 end
 
-local narrow = function(wchar, length)
-    local str_tab = {}
-    for i = 1, length do
-        str_tab[i] = string.char(wchar[i - 1])
-    end
-    return table.concat(str_tab)
-end
-
---[[
-    Alternatively expose the game's window in core
-]]
-local max_class_name = 0x10
-
-local enum_tw_cb = function(hwnd, l)
-    local wide_name = wstr_ctor(max_class_name)
-    local result_count = user32.GetClassNameW(hwnd, wide_name, max_class_name)
-    
-    assert(result_count > 0, 'Error obtaining class name for window handle.')
-
-    if narrow(wide_name, result_count) == 'FFXiClass' then
-        game_window = hwnd
-        return false
-    end
-
-    return true
-end
-
 local set_window_title = function(title)
-    assert(game_window, 'An error occurred finding the FFXI window.')
+    local game_window = ffi.cast('void*', windower.client_hwnd)
+    assert(game_window ~= nil, 'An error occurred finding the FFXI window.')
     assert(type(title) == 'string', 'The window title must be a string.')
     assert(user32.SetWindowTextW(game_window, wide(title)), 'Critical error occurred changing the window title.')
 end
 
-user32.EnumThreadWindows(kernel32.GetCurrentThreadId(), enum_tw_cb, 0)
 set_window_title(account.logged_in and account.name or 'Final Fantasy XI')
 account.login:register(function() set_window_title(account.name) end)
 account.logout:register(function() set_window_title('Final Fantasy XI') end)
