e18bd58e6c5a5c147545bc76ea1e558ff8b9c8b5
Chiaia <chiaiaxi@gmail.com>
2018-07-03 08:51:44 -0400

    Create distance.lua
    Bare Bones for now

---
+++ distance/distance.lua
@@ -0,0 +1,50 @@
+local math = require('math')
+local string = require('string')
+local ui = require('ui')
+local player = require('player')
+local target = require('target')
+
+local config_state = {
+    style = 'chromeless',
+    x = 20,
+    y = 0,
+    width = 32,
+    height = 18,
+}
+
+ui.display(function()
+    config_state = ui.window('distance', config_state, function()
+        local entity = target.t
+        
+        if entity then
+            ui.location(2, 1)
+            local distance = string.format("%.2f", (math.sqrt(entity.distance)))
+            ui.text(tostring(distance))
+        end
+    end)
+end)
+
+--[[
+Copyright © 2018, Chiaia
+All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of Chiaia nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL Chiaia BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+]]
