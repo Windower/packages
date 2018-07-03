020cf5c5bf96308886c70b5fd417c889b62ce67c
GenericHero <GenericHero@users.noreply.github.com>
2018-07-02 17:49:49 -0700

    logger: update disclaimer

--- logger/logger.lua
+++ logger/logger.lua
@@ -39,7 +39,7 @@ chat.text_added:register(function(obj)
 end)
 
 --[[
-Copyright © 2018, Windower Dev Team
+Copyright © 2018, GenericHero
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -50,14 +50,14 @@ modification, are permitted provided that the following conditions are met:
     * Redistributions in binary form must reproduce the above copyright
       notice, this list of conditions and the following disclaimer in the
       documentation and/or other materials provided with the distribution.
-    * Neither the name of the Windower Dev Team nor the
+    * Neither the name of logger nor the
       names of its contributors may be used to endorse or promote products
       derived from this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE WINDOWER DEV TEAM BE LIABLE FOR ANY
+DISCLAIMED. IN NO EVENT SHALL GenericHero BE LIABLE FOR ANY
 DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
 LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
