c2316d9f93d99bdb87ab6ffeb2be6bd258be43c8
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2018-08-05 19:21:24 +0200

    Added string require

--- reflect/reflect.lua
+++ reflect/reflect.lua
@@ -19,6 +19,11 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 --]]
+
+-- Windower modifications
+local math = require('math')
+local string = require('string')
+
 local ffi = require "ffi"
 local bit = require "bit"
 local reflect = {}
