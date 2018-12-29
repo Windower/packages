3719c9a94060ae1c30e92ffd359da8a3dbd890a5
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2018-12-30 00:00:15 +0100

    Added exceptions for Paintbrush of Souls/Geo Reservoir

--- enternity/enternity.lua
+++ enternity/enternity.lua
@@ -1,54 +1,25 @@
-local bit = require('bit')
 local chat = require('chat')
+local sets = require('sets')
 local string = require('string')
+local target = require('target')
 
-function tohex(str)
-    local i = 0
-    local out = ''
-    local key = {'1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'}
-    key[0] = '0'
-    
-    while string.len(str) > i do
-        i = i + 1
-        local val = str:byte(i)
-        local lower4 = bit.band(val,15)
-        local upper4 = bit.rshift(bit.band(val,240),4)
-        out = out..key[upper4]..key[lower4]..' '
-    end
-    return out
+local blacklist = sets({
+    'Paintbrush of Souls', -- Requires correct timing, should not be skipped
+    'Geomantic Reservoir', -- Causes dialogue freeze for some reason
+})
+
+local prompt = '\u{F800}'
+local prompt_length = #prompt
+
+do
+    local string_sub = string.sub
+
+    chat.text_added:register(function(obj)
+        if (obj.type == 150 or obj.type == 151) and string_sub(obj.text, -prompt_length) == prompt then
+            local current_target = target.t
+            if not (current_target and blacklist:contains(current_target.name)) then
+                obj.text = string_sub(obj.text, 1, -prompt_length - 1)
+            end
+        end
+    end)
 end
-
-chat.text_added:register(function(obj)
-    if obj.original_type == 150 or obj.original_type == 151 then
-        -- REVISIT : Might need to add exceptions for Paintbrush of Souls and Geomantic Reservoirs
-        obj.text = obj.text:gsub('\u{F800}','')
-    end
-end)
-
---[[
-Copyright © 2018, Windower Dev Team
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of the Windower Dev Team nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE WINDOWER DEV TEAM BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-]]
