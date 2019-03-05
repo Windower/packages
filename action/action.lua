6a9a663d0bdd99395e1416af59fb7a2a6f8eb6f2
alphaONE2 <1904368+alphaONE2@users.noreply.github.com>
2019-03-05 15:36:21 -0600

    Add action library and service

---
+++ action/action.lua
@@ -0,0 +1,50 @@
+local shared = require('shared')
+local event = require('event')
+
+local service = shared.get('action_service', 'service')
+
+local get_event = function(name)
+    local slim_event = event.slim.new()
+    service:read(name):register(function(...)
+        slim_event:trigger(...)
+    end)
+    return slim_event
+end
+
+return {
+    filter_action   = get_event('filter_action'),
+    pre_action      = get_event('pre_action'),
+    mid_action      = get_event('mid_action'),
+    post_action     = get_event('post_action'),
+    block           = function()
+        service:call(function() block() end)
+    end,
+}
+
+--[[
+Copyright © 2018, Windower Dev Team
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the Windower Dev Team nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE WINDOWER DEV TEAM BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+]]
