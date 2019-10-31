eb3d038acd06011bfbb8d4b5e6de47ff813c815c
Nekseus <38106746+Nekseus@users.noreply.github.com>
2019-10-31 13:31:17 -0400

    rewrite

--- pouches/pouches.lua
+++ pouches/pouches.lua
@@ -1,94 +1,88 @@
-local string    = require('string')
-local coroutine = require('coroutine')
+local command = require('core.command')
+local send_to_chat = require('core.chat').add_text
 
-local command   = require('core.command')
-local tolog     = require('core.chat').add_text
+local data = {}
+data.reset = function()
+    data.count = 0
+    data.delay = false
+    data.name = false
+end
 
-local inventory = require('items').bags[0]
-local player    = require('player')
+local cycle_pouches
+do
+    local player = require('player')
+    local coroutine_schedule = require('coroutine').schedule
+
+    cycle_pouches = function()
+        command.input('/item \''.. data.name ..'\' <me>')
+        data.count = data.count - 1
+
+        if data.count == 0 then
+            return coroutine_schedule(function()
+                return send_to_chat('Pouches complete...', 55)
+            end, data.delay + 3)
+        end
+
+        if player.state.id ~= 0 then
+            return send_to_chat('Pouches canceled. Player not "Idle".', 55)
+        end
+
+        return coroutine_schedule(cycle_pouches, data.delay + 2)
+    end
+end
 
 local handler
 do
-    local pouches
+    local string = require('string')
+    do
+        local string_lower = string.lower
+        local string_gsub = string.gsub
 
-    local item = {
-        count = false,
-        delay = false,
-        name  = false,
-        total = false,
-    }
-
-    local prompts = {
-        initial = function(item)
-            tolog('Pouches Inialized: '.. item.count ..' "'.. item.name ..'" found.', 2)
-            tolog('To cancel pouches change player state to anything other than "idle".', 2)
-        end,
-        no_match = function(item)
-            tolog('Pouches Error: Was unable to find item "'.. item.name ..'". '
-                ..'Check spelling and/or inventory and try again.', 2)
-        end,
-        not_idle = function(item)
-            tolog('Pouches Canceled: Player state is not "idle".', 2)
-        end,
-        unusable = function(item)
-            tolog('Pouches Error: "'.. item.name .. '" is not a useable item.', 2)
-        end,
-        complete = function(item)
-            tolog('Pouches Complete: '.. item.total ..' '.. item.name ..' used.', 2)
-        end,
-    }
-
-    pouches = function()
-        if player.state.id ~= 0 then
-            return prompts.not_idle(item)
-        end
-
-        command.input('/item "'.. item.name ..'" <me>')
-        item.count = item.count - 1
-        item.total = item.total + 1
-
-        if item.count > 0 then
-            coroutine.schedule(pouches, item.delay + 2)
-        else
-            coroutine.schedule(function()
-                return prompts.complete(item)
-            end, item.delay + 3)
+        string.squash = function(string)
+            return string_gsub(string_lower(string), '[%c%p%s]', '')
         end
     end
 
-    handler = function(line)
-        item = {
-            count = 0,
-            delay = 0,
-            name = line:lower(),
-            total = 0,
-        }
-
-        for i, k in ipairs(inventory) do
+    handler = function(item, limit)
+        data:reset()
+        local squash = item:squash()
+        for _, k in ipairs(require('items').bags[0]) do
+            -- if this table is empty then its 'nil'
             if k.item then
-                if k.item.en:lower() == item.name or k.item.enl:lower() == item.name then
-                    item.count = item.count + k.count
-                    item.delay = k.item.cast_time or false
-                    item.name = k.item.en
+                local key = k.item
+                if key.en:squash() == squash or key.enl:squash() == squash then
+                    if not key.cast_time then
+                        return send_to_chat('Error: "'.. key.en ..'" is not a usable item.')
+                    end
+
+                    if not data.delay then
+                        data.delay = key.cast_time
+                        data.name = key.en
+                    end
+
+                    data.count = data.count + k.count
+                    if limit and limit <= data.count then
+                        data.count = limit
+                        break
+                    end
                 end
             end
         end
 
-        if item.count == 0 then
-            return prompts.no_match(item)
-        elseif not item.delay then
-            return prompts.unusable(item)
+        if not data.name then
+            return send_to_chat('Error: Could not find "'.. item ..'".')
         end
-        prompts.initial(item)
-        pouches()
+
+        send_to_chat('Using '.. data.count ..' of "'.. data.name ..'". Type "/heal" to cancel.', 55)
+        return cycle_pouches()
     end
 end
 
 local pouches = command.new('pouches')
-pouches:register(handler, '<line:text>')
+pouches:register(handler, '<item:string>', '[limit:number]')
 
 --[[
-Copyright © 2019, Windower Dev Team
+Copyright © 2019; Nekseus, Windower Dev Team
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
