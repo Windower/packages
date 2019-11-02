5d1dde274b79295e66bcbaf3ca20f0246fa02a44
Nekseus <38106746+Nekseus@users.noreply.github.com>
2019-11-02 14:06:55 -0400

    update and addition to string.ext
    string slug added

--- pouches/pouches.lua
+++ pouches/pouches.lua
@@ -1,81 +1,61 @@
+local add_text = require('core.chat').add_text
 local command = require('core.command')
-local send_to_chat = require('core.chat').add_text
-
-local data = {}
-data.reset = function()
-    data.count = 0
-    data.delay = false
-    data.name = false
-end
+local coroutine_schedule = require('coroutine').schedule
+local inventory = require('items').bags[0]
+local player = require('player')
+local string = require('string.ext')
 
 local cycle_pouches
-do
-    local player = require('player')
-    local coroutine_schedule = require('coroutine').schedule
-
-    cycle_pouches = function()
-        command.input('/item \''.. data.name ..'\' <me>')
-        data.count = data.count - 1
-
-        if data.count == 0 then
-            return coroutine_schedule(function()
-                return send_to_chat('Pouches complete...', 55)
-            end, data.delay + 3)
-        end
-
-        if player.state.id ~= 0 then
-            return send_to_chat('Pouches canceled. Player not "Idle".', 55)
-        end
-
-        return coroutine_schedule(cycle_pouches, data.delay + 2)
+cycle_pouches = function(item, count, delay)
+    if player.state.id ~= 0 then
+        return
     end
+
+    command.input('/item "'.. item ..'" <me>')
+    count = count - 1
+
+    if count == 0 or player.state.id ~= 0 then
+        return
+    end
+
+    coroutine_schedule(function()
+        cycle_pouches(item, count, delay)
+    end, delay)
 end
 
-local handler
-do
-    local string = require('string')
-    do
-        local string_lower = string.lower
-        local string_gsub = string.gsub
+local handler = function(item, limit)
+    local slug = item:slug()
+    local count = 0
+    local delay
 
-        string.squash = function(string)
-            return string_gsub(string_lower(string), '[%c%p%s]', '')
-        end
-    end
+    for _, k in ipairs(inventory) do
+        if k.item then
+            local key = k.item
+            if key.name:slug() == slug or key.enl:slug() == slug then
+                if key.category ~= 'Usable' then
+                    add_text('Error: "'.. item ..'" is not a usable item.', 55)
+                    return
+                end
 
-    handler = function(item, limit)
-        data:reset()
-        local squash = item:squash()
-        for _, k in ipairs(require('items').bags[0]) do
-            -- if this table is empty then its 'nil'
-            if k.item then
-                local key = k.item
-                if key.en:squash() == squash or key.enl:squash() == squash then
-                    if not key.cast_time then
-                        return send_to_chat('Error: "'.. key.en ..'" is not a usable item.')
-                    end
+                count = count + k.count
+                delay = key.cast_time
+                item = key.name
 
-                    if not data.delay then
-                        data.delay = key.cast_time
-                        data.name = key.en
-                    end
-
-                    data.count = data.count + k.count
-                    if limit and limit <= data.count then
-                        data.count = limit
-                        break
-                    end
+                if limit and limit <= count then
+                    count = limit
+                    break
                 end
             end
         end
-
-        if not data.name then
-            return send_to_chat('Error: Could not find "'.. item ..'".')
-        end
-
-        send_to_chat('Using '.. data.count ..' of "'.. data.name ..'". Type "/heal" to cancel.', 55)
-        return cycle_pouches()
     end
+
+    if count == 0 then
+        add_text('Error: Could not find "'.. item ..'".', 55)
+        return
+    end
+
+    add_text('Using '.. count ..' of "'.. item ..'". Type "/heal" to cancel.', 55)
+    cycle_pouches(item, count, delay + 2)
 end
 
 local pouches = command.new('pouches')
