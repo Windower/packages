fb706bc9723cb05f50e5862d7c80c177ffb049df
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2019-11-04 06:42:07 +0100

    Libs/Status effects: Refactored

--- status_effects/status_effects.lua
+++ status_effects/status_effects.lua
@@ -1,113 +1,14 @@
-local shared = require('core.shared')
-local resources = require('resources')
-local enumerable = require('enumerable')
+local client = require('shared.client')
 
-local fetch_status_effects = shared.get('status_effects_service', 'status_effects')
+local data, ftype = client.new('status_effects_service')
 
-local iterate = function(data, resource_name, index)
-    return next(data[resource_name], index)
-end
-
-local player_indexer = function(result, index)
-    local data = {}
-    local data_count = 0
-    for _, v in pairs(result) do
-        if v.id == index then
-            data_count = data_count + 1
-            data[data_count] = v
-        end
-    end
-    return data
-end
-
-local party_indexer = function(result, index)
-    local data = {}
-    local data_count = 0
-    for _, v in pairs(result) do
-        if v == index then
-            data_count = data_count + 1
-            data[data_count] = v
-        end
-    end
-    return data
-end
-
-local indexers = {
-    party = function(result, index)
-        if not result[index] then
-            return
-        end
-
-        return setmetatable({}, {
-            __index = function(_, k)
-                if type(k) == 'string' then
-                    local status = resources.buffs:first(function(v) return v.en == k end)
-                    if status then
-                        return party_indexer(result[index], status.id)
-                    end
-                elseif type(k) == 'number' then
-                    local status = resources.buffs[k]
-                    if status then
-                        return party_indexer(result[index], status.id)
-                    end
-                end
-            end,
-            __len = function(_)
-                return #result[index]
-            end,
-            __pairs = function(_)
-                return function(_, k)
-                    return next(result[index], k)
-                end
-            end,
-        })
-    end,
-    player = function(result, index)
-        if type(index) == 'string' then
-            local status = resources.buffs:first(function(v) return v.en == index end)
-            if status then
-                return player_indexer(result, status.id)
-            end
-        elseif type(index) == 'number' then
-            local status = resources.buffs[index]
-            if status then
-                return player_indexer(result, status.id)
-            end
-        end
+ftype.fields.player = {
+    get = function(data)
+        return data.party[0]
     end,
 }
 
-local constructors = setmetatable({}, {
-    __index = function(mts, resource_name)
-        local meta = {}
-
-        meta.__index = function(_, index)
-            if indexers[resource_name] then
-                local indexed = indexers[resource_name](fetch_status_effects:read(resource_name), index)
-                if indexed then
-                    return indexed
-                end
-            end
-            return fetch_status_effects:read(resource_name, index)
-        end
-
-        meta.__pairs = function(t)
-            return function(_, index)
-                return fetch_status_effects:call(iterate, resource_name, index)
-            end, t, nil
-        end
-
-        local constructor = enumerable.init_type(meta)
-        mts[resource_name] = constructor
-        return constructor
-    end,
-})
-
-return setmetatable({}, {
-    __index = function(_, resource_name)
-        return constructors[resource_name]()
-    end
-})
+return data
 
 --[[
 Copyright © 2018, Windower Dev Team
