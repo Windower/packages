9123c431bac749912572dc498fbfe0c39045c89b
alphaONE2 <1904368+alphaONE2@users.noreply.github.com>
2020-05-25 18:27:11 -0500

    Fix deploy.ps1... again

--- .github/scripts/deploy.ps1
+++ .github/scripts/deploy.ps1
@@ -6,6 +6,7 @@ $repo = ($env:GITHUB_REPOSITORY -Split '/')[1] ?? $env:GITHUB_REPOSITORY
 &scp -r './.staging/*' "${env:SSH_USER}@${env:SSH_HOST}:~/staging/${repo}"
 &ssh "${env:SSH_USER}@${env:SSH_HOST}" (@(
     "rm -rf ~/backup/"
+    "mkdir ~/backup/"
     "mv ${env:FILES_PATH}/* ~/backup/",
     "mv ~/staging/${repo}/* ${env:FILES_PATH}/",
     "rm -r ~/backup/"
