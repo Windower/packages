c9b5908398397be411ebc20f3f06f98ab9de8961
alphaONE2 <1904368+alphaONE2@users.noreply.github.com>
2020-05-26 18:21:00 -0500

    Fix missing or incorrect licenses

--- addons/dress_up/dress_up.lua
+++ addons/dress_up/dress_up.lua
@@ -245,8 +245,10 @@ end)
 --[[
 Copyright © 2019, Windower Dev Team
 All rights reserved.
+
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
+
     * Redistributions of source code must retain the above copyright
       notice, this list of conditions and the following disclaimer.
     * Redistributions in binary form must reproduce the above copyright
@@ -255,7 +257,8 @@ modification, are permitted provided that the following conditions are met:
     * Neither the name of the Windower Dev Team nor the
       names of its contributors may be used to endorse or promote products
       derived from this software without specific prior written permission.
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' AND
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
 DISCLAIMED. IN NO EVENT SHALL THE WINDOWER DEV TEAM BE LIABLE FOR ANY
