c834024f6f6748c330ad118d6353d8731c886cc0
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2020-05-30 00:26:35 +0200

    Items: Added lookup functions

--- libraries/items_service/items_service.lua
+++ libraries/items_service/items_service.lua
@@ -1,8 +1,12 @@
 local account = require('account')
+local channel = require('core.channel')
+local client_data_items = require('client_data.items')
 local packet = require('packet')
 local resources = require('resources')
 local server = require('shared.server')
+local string = require('string.ext')
 local struct = require('struct')
+local table = require('table')
 
 local item_type = struct.struct({
     id                  = {struct.int32},
@@ -49,42 +53,132 @@ end
 struct.reset_on(account.logout, equipment, equipment_type)
 struct.reset_on(account.logout, items, items_type)
 
+local search_server = channel.new('items_service_search')
+search_server.data = {}
+
+local normalized_lookup
+local init_search = function()
+    normalized_lookup = {}
+    search_server.data.search_map = normalized_lookup
+end
+init_search()
+
+search_server.data.id_map = {}
+
+local id_names = {}
+local id_full_names = {}
+
+do
+    local string_normalize = string.normalize
+    local id_map = search_server.data.id_map
+
+    for i = 0, 0xEFFF do
+        local item = client_data_items[i]
+        if item ~= nil then
+            local item_name = item.name
+            if item_name ~= '.' then
+                local name = string_normalize(item_name)
+                local full_name = string_normalize(item.full_name)
+                local id = item.id
+                id_names[id] = name
+                id_full_names[id] = full_name
+
+                local id_map_name = id_map[name]
+                if id_map_name == nil then
+                    id_map_name = {}
+                    id_map[name] = id_map_name
+                end
+                id_map_name[#id_map_name + 1] = id
+
+                local id_map_full_name = id_map[full_name]
+                if id_map_full_name == nil then
+                    id_map_full_name = {}
+                    id_map[full_name] = id_map_full_name
+                end
+                id_map_full_name[#id_map_full_name + 1] = id
+            end
+        end
+    end
+end
+
+account.logout:register(init_search)
+
 local empty_item = struct.new(item_type)
 
-local update_item = function(bag, index, count, status, id, bazaar, extdata)
-    if bag == 0 and index == 0 then
-        items.gil = count
-        return
-    end
+local update_item
+do
+    local table_remove = table.remove
 
-    local item = items.bags[bag][index]
-    item.count = count
-    item.status = status
-
-    if count == 0 then
-        item.id = 0
-        item.bazaar = 0
-        item.extdata = empty_item.extdata
-
-        item = empty_item
-    else
-        if id then
-            item.id = id
+    update_item = function(bag, index, count, status, id, bazaar, extdata)
+        if bag == 0 and index == 0 then
+            items.gil = count
+            return
         end
-        if bazaar then
-            item.bazaar = bazaar
-        end
-        if extdata then
-            item.extdata = extdata
-        end
-    end
 
-    local slot = equipment_references[bag][index]
-    if slot then
-        equipment[slot].item = item
+        local item = items.bags[bag][index]
+        item.count = count
+        item.status = status
 
         if count == 0 then
-            equipment_references[bag][index] = nil
+            if item.id ~= 0 then
+                local lookup = normalized_lookup[id_full_names[item.id]]
+                if lookup ~= nil then
+                    for i = 1, #lookup do
+                        local entry = lookup[i]
+                        if entry[1] == bag and entry[2] == index then
+                            table_remove(lookup, i)
+                            break
+                        end
+                    end
+                end
+            end
+            item.id = 0
+            item.bazaar = 0
+            item.extdata = empty_item.extdata
+
+            item = empty_item
+        else
+            if id then
+                local old_id = item.id
+                if old_id ~= id then
+                    local old_lookup = normalized_lookup[id_full_names[old_id]]
+                    if old_lookup ~= nil then
+                        for i = 1, #old_lookup do
+                            local entry = old_lookup[i]
+                            if entry[1] == bag and entry[2] == index then
+                                table_remove(old_lookup, i)
+                                break
+                            end
+                        end
+                    end
+
+                    local new_name = id_full_names[id]
+                    local lookup = normalized_lookup[new_name]
+                    if not lookup then
+                        lookup = {}
+                        normalized_lookup[new_name] = lookup
+                        normalized_lookup[id_names[id]] = lookup
+                    end
+
+                    lookup[#lookup + 1] = {bag, index}
+                end
+                item.id = id
+            end
+            if bazaar then
+                item.bazaar = bazaar
+            end
+            if extdata then
+                item.extdata = extdata
+            end
+        end
+
+        local slot = equipment_references[bag][index]
+        if slot then
+            equipment[slot].item = item
+
+            if count == 0 then
+                equipment_references[bag][index] = nil
+            end
         end
     end
 end
