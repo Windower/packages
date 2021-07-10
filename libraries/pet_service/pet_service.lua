277d5381e0e6f81bf71d6332eea48d68c61a7575
John S Hobart <sigilbaram@gmail.com>
2021-07-10 19:57:58 -0400

    fix license

--- libraries/pet_service/pet_service.lua
+++ libraries/pet_service/pet_service.lua
@@ -151,10 +151,12 @@ packet.incoming:register_init({
 })
 
 --[[
-Copyright © 2020, John S Hobart
+Copyright © 2020, Windower Dev Team
 All rights reserved.
+
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
+
     * Redistributions of source code must retain the above copyright
       notice, this list of conditions and the following disclaimer.
     * Redistributions in binary form must reproduce the above copyright
@@ -163,6 +165,7 @@ modification, are permitted provided that the following conditions are met:
     * Neither the name of the Windower Dev Team nor the
       names of its contributors may be used to endorse or promote products
       derived from this software without specific prior written permission.
+
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
