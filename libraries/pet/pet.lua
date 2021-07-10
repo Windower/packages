b41b1295914bb35ba60d2dceb2d424e52219b1de
John S Hobart <sigilbaram@gmail.com>
2021-07-09 21:48:07 -0400

    fix copyright date

--- libraries/pet/pet.lua
+++ libraries/pet/pet.lua
@@ -52,7 +52,7 @@ ftype.fields.automaton.type.fields.attach_available = {
 return data
 
 --[[
-Copyright © 2018, John S Hobart
+Copyright © 2020, John S Hobart
 All rights reserved.
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
