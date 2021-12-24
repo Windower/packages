5cf5d389b648d0fa472ca4353d42eca7b5d956a8
Nifim <tesarollens@gmail.com>
2021-12-23 17:39:36 -0800

    Fix date

--- addons/distance/distance.lua
+++ addons/distance/distance.lua
@@ -29,7 +29,7 @@ ui.display(function()
 end)
 
 --[[
-Copyright © 2018, 2020 Chiaia
+Copyright © 2018, 2021 Chiaia
 All rights reserved.
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
